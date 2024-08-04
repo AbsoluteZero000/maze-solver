04dea7ac6c6ee1525686caebf291be67e9c90718
AbsoluteZero000 <ahmedwaelwanas@gmail.com>
2024-08-04 09:35:34 +0300

    added random seed for both

--- src/main.py
+++ src/main.py
@@ -1,22 +1,27 @@
 import threading
 from maze import Maze
 from graphics import Window
-
-def run_dfs_solver():
-    maze1 = Maze(100, 100, 20, 20, 30, 30, Window(800, 800, "DFS Maze Solver"), 2020)
+from time import sleep
+from random import randint
+def run_dfs_solver(seed):
+    maze1 = Maze(100, 100, 20, 20, 30, 30, Window(800, 800, "DFS Maze Solver"), seed)
     maze1._solve_dfs(0, 0)
+    sleep(100)
     maze1._win.wait_for_close()
 
 
-def run_bfs_solver():
-    maze2 = Maze(100, 100, 20, 20, 30, 30, Window(800, 800, "BFS Maze Solver"), 2020)
+def run_bfs_solver(seed):
+    maze2 = Maze(100, 100, 20, 20, 30, 30, Window(800, 800, "BFS Maze Solver"), seed)
     maze2._solve_bfs(0, 0)
+    sleep(100)
     maze2._win.wait_for_close()
 
 
+
 def main():
-    dfs_thread = threading.Thread(target=run_dfs_solver)
-    bfs_thread = threading.Thread(target=run_bfs_solver)
+    seed = randint(0, 10000)
+    dfs_thread = threading.Thread(target=run_dfs_solver, args=(seed,))
+    bfs_thread = threading.Thread(target=run_bfs_solver, args=(seed,))
 
     dfs_thread.start()
     bfs_thread.start()
